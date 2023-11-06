4ef0ccd209db757e206fe6a7000a8d1faca08ff7
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-11-06 13:49:19 +0000

    Create twoSums.js

---
+++ leetcode-practice/twoSums.js
@@ -0,0 +1,20 @@
+function twoSums(nums, target) {
+    //  return indices of the two numbers such that they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    // You can return the answer in any order.
+
+}
+
+// const nums = [2, 7, 11, 15];
+// const target = 9;
+// Output: [0,1]
+
+const nums = [3, 2, 4];
+const target = 6;
+// Output: [1,2]
+
+// const nums = [3, 3];
+// const target = 6;
+// Output: [0,1]
+
+console.log(twoSums(nums, target));
